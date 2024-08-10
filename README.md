edfdae6242901c8373542dfb1ac0912a1862550a
aleksblaze <alekshak232@gmail.com>
2024-08-11 00:22:05 +0300

    taskendar lesson 9

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-# taskendar
+# Taskendar
+**Taskendar** is a calendar where you can add all your tasks and check up on their completion. The application was created to make it easier for you to keep track of your tasks and daily routine.
 
-A calendar where you can add all your tasks and check their completion.
+## App work
+![image](https://github.com/user-attachments/assets/86bc2eae-a70c-475b-8506-0880d12302cf)
+
+## App scheme
+![image](https://github.com/user-attachments/assets/6a6ad36c-1f61-450e-95a5-0a5c9e7c5afe)
+
+
+## Requirements
+
+To use **Taskendar**, the following requierements aplly:
+- a phone 
+- another phone (in case u wanna use it on both ur phones)*
+
+## Installation
+The installation guide will definitely be here once I figure out how to create one.
+
+## Usage
+You open the app, click on any day you want to add a task on, press "Add a task" or whatever name I give it and choose the task. Volla! You have added a task! Now when that day comes you will receive a notification.
+
+## Contact
+If you'll have any problems using Taskendar contact me through:
+
+**Telegram** - @aleksblaze
+
+**Email** - alekshak232@gmail.com
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+const AppTitle = 'Taskendar';
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Taskendar',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text('Taskendar'),
+            ),
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Tasks'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
       ),
     );
