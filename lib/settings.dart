a72cb7dc5854999b4bdd23cd433f9933ce9a21fc
aleksblaze <alekshak232@gmail.com>
2024-10-20 14:53:16 +0300

    as

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:taskendar/models/themeProvider.dart';
+import 'package:taskendar/models/theme_provider.dart';
 import 'package:taskendar/main.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
